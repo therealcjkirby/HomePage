2e516ba17d03975299a8b86eb7ae7cecadbdff27
CJ Kirby <125667511+therealcjkirby@users.noreply.github.com>
2025-02-15 15:15:27 +0930

    update 2

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const CONFIG = {
 	name: 'CJ 😊',
 	imageBackground: false,
 	openInNewTab: true,
-	twelveHourFormat: false,
+	twelveHourFormat: true,
 
 	// Greetings
 	greetingMorning: 'Good morning!',
